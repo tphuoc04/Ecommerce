5ae9fc5ffb50413270f39c513a9fedc6e15e6d40
tphuoc04 <phuoctong2004@gmail.com>
2024-06-12 19:37:17 +0700

    project init

--- src/main/java/com/ecommerce/E_commerce/HelloController.java
+++ src/main/java/com/ecommerce/E_commerce/HelloController.java
@@ -1,13 +1,16 @@
 package com.ecommerce.E_commerce;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class HelloController {
     
     @RequestMapping("/")
     public String index(){
-        return "index";
+        return "Hello World";
     }
 }
