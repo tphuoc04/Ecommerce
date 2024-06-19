4a41225333b05a21cbbc7180c80c65acfa0975a9
tphuoc04 <phuoctong2004@gmail.com>
2024-06-19 23:44:41 +0700

    thymeleaf

--- src/main/java/com/ecommerce/E_commerce/HelloController.java
+++ src/main/java/com/ecommerce/E_commerce/HelloController.java
@@ -1,37 +1,33 @@
 package com.ecommerce.E_commerce;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-interface Item {
-    String title = null;
-    String description = null;
-}
-
 @Controller
 public class HelloController {
     @RequestMapping("/")
     public String index(Model model){
-        List<Item> items = new ArrayList<Item>();
+        List<Object> items = new ArrayList<Object>();
 
-        Item item1 = new Item(){
-            String title = "Item 1";
-            String description = "Description 1";
-        };
+        HashMap item1 = new HashMap();
+        item1.put("title", "Item 1");
+        item1.put("description", "Description 1");
 
-        Item item2 = new Item(){
-            String title = "Item 2";
-            String description = "Description 2";
-        };
+        HashMap item2 = new HashMap();
+        item2.put("title", "Item 2");
+        item2.put("description", "Description 2");
 
         items.add(item1);
         items.add(item2);
         System.out.print(items);
+        
         model.addAttribute("items", items);
+
         return "index";
     }
 
