82b099568e36315d05c0a3ffa28d33be7ee555f5
tphuoc04 <phuoctong2004@gmail.com>
2024-06-19 22:47:13 +0700

    Really logo

--- src/main/java/com/ecommerce/E_commerce/HelloController.java
+++ src/main/java/com/ecommerce/E_commerce/HelloController.java
@@ -1,16 +1,42 @@
 package com.ecommerce.E_commerce;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+interface Item {
+    String title = null;
+    String description = null;
+}
 
-@RestController
+@Controller
 public class HelloController {
-    
     @RequestMapping("/")
-    public String index(){
-        return "Hello World";
+    public String index(Model model){
+        List<Item> items = new ArrayList<Item>();
+
+        Item item1 = new Item(){
+            String title = "Item 1";
+            String description = "Description 1";
+        };
+
+        Item item2 = new Item(){
+            String title = "Item 2";
+            String description = "Description 2";
+        };
+
+        items.add(item1);
+        items.add(item2);
+        System.out.print(items);
+        model.addAttribute("items", items);
+        return "index";
+    }
+
+    @RequestMapping("/category")
+    public String category(){
+        return "category";
     }
 }
